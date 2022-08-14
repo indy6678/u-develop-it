9bcdd7ff171c172adb6e6b16199ff0e41f870c58
indy6678 <indy6678@gmail.com>
2022-08-14 01:35:09 -0500

    end of 12.2.4

--- server.js
+++ server.js
@@ -35,10 +35,22 @@ const db = mysql.createConnection(
 // });
 
 // delete a single candidate
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }  
+//     console.log(result);
+// });
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
     if(err) {
         console.log(err);
-    }  
+    }
     console.log(result);
 });
 
